c311c692e1b20a56ae10b7a4113badd45db5512d
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2022-01-29 13:52:33 -0500

    database stuff

--- src/hacks_flask/app.py
+++ src/hacks_flask/app.py
@@ -1,17 +1,18 @@
 from flask import Flask, session, abort, redirect
 import sys, os
-
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 import core_gmaps
 import core_sentimenta
 import core_twilio
 import blueprint_auth
+import core_firestore
 
 import blueprint_get
 import blueprint_auth
 
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
 
 # register get blueprint
 app.register_blueprint(blueprint_get.get_flask_blueprint)
--- src/hacks_flask/core_firestore.py
+++ src/hacks_flask/core_firestore.py
@@ -1,11 +1,12 @@
 from google.cloud import firestore
+
+
 import os
 # The `project` parameter is optional and represents which project the client
 # will act on behalf of. If not supplied, the client falls back to the default
 # project inferred from the environment.
-db = firestore.Client()
-
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath("firestore_keys.json")
+db = firestore.Client(project='alien-segment-337020')
 
 doc_ref = db.collection(u'users').document(u'aturing')
 doc_ref.set({
