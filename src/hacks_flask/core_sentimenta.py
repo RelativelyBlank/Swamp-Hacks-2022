1457f27297d417b89c0803baa4ead3d0213bd6be
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2022-01-29 03:21:51 -0500

    updated sentiment

--- src/hacks_flask/core_sentimenta.py
+++ src/hacks_flask/core_sentimenta.py
@@ -6,20 +6,50 @@ credentials = os.path.abspath('keys.json')
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=credentials
 
 from google.cloud import language_v1
+def sample_analyze_sentiment(text_content):
+    """
+    Analyzing Sentiment in a String
 
-# Instantiates a client
-client = language_v1.LanguageServiceClient()
+    Args:
+      text_content The text content to analyze
 
-# The text to analyze
-text = u"Hello, world!"
-document = language_v1.Document(
-    content=text, type_=language_v1.Document.Type.PLAIN_TEXT
-)
+    read this:
+    https://cloud.google.com/natural-language/docs/basics#interpreting_sentiment_analysis_values
+    """
 
-# Detects the sentiment of the text
-sentiment = client.analyze_sentiment(
-    request={"document": document}
-).document_sentiment
+    client = language_v1.LanguageServiceClient()
 
-print("Text: {}".format(text))
-print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
+    # text_content = 'I am so happy and joyful.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
+    # Get overall sentiment of the input document
+    print(u"Document sentiment score: {}".format(response.document_sentiment.score))
+    print(
+        u"Document sentiment magnitude: {}".format(
+            response.document_sentiment.magnitude
+        )
+    )
+    # Get sentiment for all sentences in the document
+    for sentence in response.sentences:
+        print(u"Sentence text: {}".format(sentence.text.content))
+        print(u"Sentence sentiment score: {}".format(sentence.sentiment.score))
+        print(u"Sentence sentiment magnitude: {}".format(sentence.sentiment.magnitude))
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print(u"Language of the text: {}".format(response.language))
+
+sample_analyze_sentiment("I am so happy and joyful.")
