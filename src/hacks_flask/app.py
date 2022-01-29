e4696763cc2a2bbd8819aea21a0fb94dd31295b5
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2022-01-29 17:38:23 -0500

    Update app.py

--- src/hacks_flask/app.py
+++ src/hacks_flask/app.py
@@ -1,6 +1,24 @@
 from flask import Flask, session, abort, redirect
 import sys, os
 import datetime
+# ==== INSTRUCTIONS ====
+# TO GET THE IMAGE OF THE LOCATION
+#   -> SEND A REQUEST TO http://127.0.0.1:5000/location/<location>/image
+#   -> THE IMAGE IS DOWNLOADED TO imgs/<location>.jpg
+# TO GET THE BEST REVIEW OF THE LOCATION
+#   -> SEND A REQUEST TO http://127.0.0.1:5000/location/<location>/best_review
+#   -> RETURNS A JSON WITH THE BEST REVIEW
+# TO UPLOAD A FILE:
+#   -> SEND A REQUEST TO http://127.0.0.1:5000/post/upload_file
+#   -> THE JSON MUST BE LIKE '{ "image_location": "imgs/test1.jpg", "email" : "salvadoraleguas@gmail.com" }'
+#   -> THE FILE MUST BE SENT AS A POST REQUEST
+# TO GET AN IMAGE FROM FIREBASE
+#  -> SEND A REQUEST TO http://127.0.0.1:5000/get/<email>/get_file/<image_name>
+#  -> THE IMAGE IS DOWNLOADED TO imgs/<image_name>
+# TO GET ALL THE IMAGES FOR A USER ON FIREBASE
+#  -> SEND A REQUEST TO http://127.0.0.1:5000/get/<email>/get_images
+#  -> RETURNS A JSON OF THE IMAGE NAMES
+
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 import core_gmaps
