b1e24ed7a7aae71c996e7726ca14235f3589d964
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2022-01-30 08:30:20 -0500

    lol

--- src/hacks_flask/core_firestore.py
+++ src/hacks_flask/core_firestore.py
@@ -25,6 +25,7 @@ def upload_image_to_firebase(image_file_path, email):
 #     return an html response that indicates the upload was successful
     return "upload successful"
 
+upload_image_to_firebase(os.path.abspath('imgs/eiffel tower.jpg'), 'salvadoraleguas@gmail.com')
 def get_image_from_firebase(image_name, email):
     # check if session exists
 
